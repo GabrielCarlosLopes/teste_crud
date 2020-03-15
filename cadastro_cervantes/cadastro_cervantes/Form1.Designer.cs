46beef774a6f80e1d7d79ca63a189d667f48c9a6
gabrielcarloslopes <gabrielcarloslopes1999@gmail.com>
2020-03-15 19:39:55 -0300

    update_crud

---
+++ cadastro_cervantes/cadastro_cervantes/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace cadastro_cervantes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textDescricao = new System.Windows.Forms.TextBox();
+            this.textNumerico = new System.Windows.Forms.TextBox();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Descrição";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numérico";
+            // 
+            // textDescricao
+            // 
+            this.textDescricao.Location = new System.Drawing.Point(101, 27);
+            this.textDescricao.Name = "textDescricao";
+            this.textDescricao.Size = new System.Drawing.Size(377, 20);
+            this.textDescricao.TabIndex = 1;
+            // 
+            // textNumerico
+            // 
+            this.textNumerico.Location = new System.Drawing.Point(101, 60);
+            this.textNumerico.Name = "textNumerico";
+            this.textNumerico.Size = new System.Drawing.Size(190, 20);
+            this.textNumerico.TabIndex = 2;
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToAddRows = false;
+            this.dgvData.AllowUserToDeleteRows = false;
+            this.dgvData.AllowUserToOrderColumns = true;
+            this.dgvData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(27, 120);
+            this.dgvData.MultiSelect = false;
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvData.Size = new System.Drawing.Size(717, 230);
+            this.dgvData.TabIndex = 7;
+            this.dgvData.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvData_CellClick);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(297, 59);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(56, 23);
+            this.btnInsert.TabIndex = 3;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(359, 59);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(56, 23);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(421, 59);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(56, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(483, 60);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(56, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(768, 362);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.textNumerico);
+            this.Controls.Add(this.textDescricao);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "   Cadastro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textDescricao;
+        private System.Windows.Forms.TextBox textNumerico;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ cadastro_cervantes/cadastro_cervantes/Form1.cs
@@ -0,0 +1,183 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cadastro_cervantes
+{
+    public partial class Form1 : Form
+    {
+
+        private string connstring = String.Format("Server={0};Port={1};" +
+            "User Id={2};Password={3};Database={4};",
+            "localhost", 5432, "postgres",
+            "1234", "cervantes_teste"
+            );
+
+        private NpgsqlConnection conn;
+        private string sql;
+        private NpgsqlCommand cmd;
+        private DataTable dt;
+        private int rowIndex = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new NpgsqlConnection(connstring);
+            Select();
+        }
+
+        private void Select()
+        {
+            try
+            {
+                conn.Open();
+                sql = @"select * from selectCadastro()";
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                conn.Close();
+                dgvData.DataSource = null;
+                dgvData.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show("Error"+ex.Message);
+            }
+        }
+
+        private void dgvData_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex>=0)
+            {
+                rowIndex = e.RowIndex;
+                textDescricao.Text = dgvData.Rows[e.RowIndex].Cells["descrição"].Value.ToString();
+                textNumerico.Text = dgvData.Rows[e.RowIndex].Cells["numérico"].Value.ToString();
+            }
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            rowIndex = -1;
+            textDescricao.Enabled = textNumerico.Enabled = true;
+            textDescricao.Text = textNumerico.Text = null;
+            textDescricao.Select();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if(rowIndex < 0)
+            {
+                MessageBox.Show("please choose a register to update");
+                return;
+            }
+            textDescricao.Enabled = textNumerico.Enabled = true;
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (rowIndex < 0)
+            {
+                MessageBox.Show("please choose a register to delete");
+                return;
+            }
+            try
+            {
+                conn.Open();
+                sql = @"select * from deleteCadastro(:_id)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_id", int.Parse(dgvData.Rows[rowIndex].Cells["idcadastro"].Value.ToString()));
+                if ((int)cmd.ExecuteScalar() == 1)
+                {
+                    MessageBox.Show("Delete register success");
+                    rowIndex = -1;
+                    conn.Close();
+                    Select();
+                }
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show("Delete fail. Error: " + ex.Message);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            int result = 0;
+            if(rowIndex < 0) // insert
+            {
+                try
+                {
+                    conn.Open();
+                    sql = @"select * from insertCadastro(:descricao, :num)";
+                    cmd = new NpgsqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("descricao", textDescricao.Text);
+                    cmd.Parameters.AddWithValue("num", int.Parse(textNumerico.Text));
+                    result = (int)cmd.ExecuteScalar();
+                    conn.Close();
+                    if(result == 1)
+                    {
+                        MessageBox.Show("Insert new register success");
+                        Select();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Insert fail");
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    conn.Close();
+                    MessageBox.Show("Insert fail. Error" + ex.Message);
+                }
+            }
+            else // update
+            {
+                try
+                {
+                    conn.Open();
+                    sql = @"select * from updateCadastro(:_id, :descricaonew, :numnew)";
+                    cmd = new NpgsqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("_id", int.Parse(dgvData.Rows[rowIndex].Cells["idCadastro"].Value.ToString()));
+                    cmd.Parameters.AddWithValue("descricaonew", textDescricao.Text);
+                    cmd.Parameters.AddWithValue("numnew", int.Parse(textNumerico.Text));
+                    result = (int)cmd.ExecuteScalar();
+                    conn.Close();
+                    if(result == 1)
+                    {
+                        MessageBox.Show("Update success");
+                        Select();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Update fail");
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    conn.Close();
+                    MessageBox.Show("Update fail. Error" + ex.Message);
+                }
+            }
+            result = 0;
+            textDescricao.Text = textNumerico.Text = null;
+            textDescricao.Enabled = textNumerico.Enabled = false;
+        }
+    }
+}
